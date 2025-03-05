554e9c7bf4ed06090ac66d983db812d6e4bae412
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-05 23:49:23 +0530

    Practice event listeners

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-document.querySelector("button").addEventListener("click" , wBtnClick);
-
-function wBtnClick(){
-    alert("w button clicked");
+var i;
+for(i=0;i<=7;i++){
+    document.querySelectorAll("button")[i].addEventListener("click" , function(){
+        alert("You clicked me");
+    });    
 }
+
