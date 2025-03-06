e052dd776a11d71e7f3636cbac19c1620b25d6b8
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-06 13:59:29 +0530

    practice "this" in  js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
     
     for(var i=0; i<7; i++){
         document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-            var audio = new Audio("tom1.wav");
-            audio.play();
+            this.style.color = "white";
         });
-
     }
+
+
+    // var audio = new Audio("tom1.wav");
+    //         audio.play();
+       
     
      
 
