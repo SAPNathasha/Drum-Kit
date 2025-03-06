5c61944159e088f4e43a38a9d8a85813c3dfa9df
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-06 12:20:35 +0530

    set interface

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var i;
-for(i=0; i<=7; i++){
-    document.querySelectorAll("button")[i].addEventListener("click" , function(){
-        alert("You clicked me");
-    });    
-}
+
+    
+    for(var i=0; i<7; i++){
+        document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+            var audio = new Audio("tom1.wav");
+            audio.play();
+        });
+
+    }
+    
+     
+
 
